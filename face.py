68cbde8ac558feb7746334630b8b61316712820a
= <=>
2018-08-08 13:47:34 +0100

    Key added in error

--- face.py
+++ face.py
@@ -45,8 +45,8 @@ try:
     #    cv2.imshow("Frame", frame)
     #    key = cv2.waitKey(1) & 0xFF
 
-        if key == ord("q"):
-            break
+        #if key == ord("q"):
+        #    break
 
     #cv2.destroyAllWindows()
     vs.stop()
