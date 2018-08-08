ed6b311af2708d95a5fe6e8957a087f5e1b4fe7b
= <=>
2018-08-08 15:19:39 +0100

    removing UI and face xml

--- face.py
+++ face.py
@@ -39,16 +39,8 @@ try:
 
             text = "{:.2f}%".format(confidence * 100)
             y = startY - 10 if startY - 10 > 10 else startY + 10
-    #        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-    #        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
             print(text);
-    #    cv2.imshow("Frame", frame)
-    #    key = cv2.waitKey(1) & 0xFF
 
-        #if key == ord("q"):
-        #    break
-
-    #cv2.destroyAllWindows()
     vs.stop()
 
 
