0a22d34d08ce4ab6e872f929a2c367426613cb0d
= <=>
2018-08-08 13:46:32 +0100

    No screen just output visible face positions

--- face.py
+++ face.py
@@ -1,4 +1,5 @@
 from imutils.video import VideoStream
+from imutils.video import FPS
 import unicornhathd
 import imutils
 import numpy as np
@@ -38,16 +39,16 @@ try:
 
             text = "{:.2f}%".format(confidence * 100)
             y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+    #        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    #        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            print(text);
+    #    cv2.imshow("Frame", frame)
+    #    key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
             break
 
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
     vs.stop()
 
 
